ae27bd8db318c7e481f551282319b7e6771fa808
Yuki Kishimoto <yukikishimoto@proton.me>
2022-09-15 23:04:45 +0200

    Update client example

--- examples/client.rs
+++ examples/client.rs
@@ -1,27 +1,27 @@
 // Copyright (c) 2021-2022 Yuki Kishimoto
 // Distributed under the MIT software license
 
-extern crate braiinspool;
-
-use braiinspool::Client;
+use braiinspool::{Client, Error};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     // Init client
-    let client = Client::new("apikey", Some("socks5://127.0.0.1:9050")).unwrap();
+    let client = Client::new("apikey", Some("socks5h://127.0.0.1:9050"))?;
 
     // Check tor connection
-    println!("{:#?}", client.check_tor_connection().await.unwrap());
+    println!("{}", client.check_tor_connection().await?);
 
     // Get pool stats
-    println!("{:#?}", client.pool_stats().await.unwrap());
+    println!("{:#?}", client.pool_stats().await?);
 
     // Get user profile
-    println!("{:#?}", client.user_profile().await.unwrap());
+    println!("{:#?}", client.user_profile().await?);
 
     // Get daily rewards
-    println!("{:#?}", client.daily_rewards().await.unwrap());
+    println!("{:#?}", client.daily_rewards().await?);
 
     // Get workers
-    println!("{:#?}", client.workers().await.unwrap());
+    println!("{:#?}", client.workers().await?);
+
+    Ok(())
 }
