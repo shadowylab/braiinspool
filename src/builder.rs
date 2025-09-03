a0273d2cb3114507c90f41fc14b04ef85661c9c6
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-09-03 12:32:55 +0200

    Add socks proxy support
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- src/builder.rs
+++ src/builder.rs
@@ -1,7 +1,11 @@
 //! Braiins Pool client builder
 
+#[cfg(feature = "socks")]
+use std::net::SocketAddr;
 use std::time::Duration;
 
+#[cfg(feature = "socks")]
+use reqwest::Proxy;
 use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::{Client, ClientBuilder};
 
@@ -13,8 +17,13 @@ const DEFAULT_TIMEOUT: Duration = Duration::from_secs(60);
 /// Braiins Pool client builder
 #[derive(Debug, Clone)]
 pub struct BraiinsPoolClientBuilder {
-    api_key: String,
-    timeout: Duration,
+    /// API key
+    pub api_key: String,
+    /// Timeout
+    pub timeout: Duration,
+    /// Socks5 proxy
+    #[cfg(feature = "socks")]
+    pub proxy: Option<SocketAddr>,
 }
 
 impl BraiinsPoolClientBuilder {
@@ -26,12 +35,22 @@ impl BraiinsPoolClientBuilder {
         Self {
             api_key: api_key.into(),
             timeout: DEFAULT_TIMEOUT,
+            #[cfg(feature = "socks")]
+            proxy: None,
         }
     }
 
     /// Set timeout (default: 60 sec)
     pub fn timeout(mut self, timeout: Duration) -> Self {
         self.timeout = timeout;
+        self
+    }
+
+    /// Set proxy
+    #[inline]
+    #[cfg(feature = "socks")]
+    pub fn proxy(mut self, proxy: SocketAddr) -> Self {
+        self.proxy = Some(proxy);
         self
     }
 
@@ -43,11 +62,22 @@ impl BraiinsPoolClientBuilder {
         let mut headers: HeaderMap = HeaderMap::with_capacity(1);
         headers.insert("Pool-Auth-Token", auth_value);
 
-        let builder: ClientBuilder = Client::builder()
-            .default_headers(headers)
-            .timeout(self.timeout);
+        let mut builder: ClientBuilder = Client::builder().default_headers(headers);
+
+        // Set timeout
+        builder = builder.timeout(self.timeout);
+
+        // Set proxy
+        #[cfg(all(feature = "socks", not(target_arch = "wasm32")))]
+        if let Some(proxy) = self.proxy {
+            let proxy: String = format!("socks5h://{proxy}");
+            builder = builder.proxy(Proxy::all(proxy)?);
+        }
+
+        // Build client
         let client: Client = builder.build()?;
 
+        // Construct client
         Ok(BraiinsPoolClient::from_client(client))
     }
 }
